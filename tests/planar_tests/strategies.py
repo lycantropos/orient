12d9ccfe6b71dc37fb3385fc3a085cf90104a313
Ibrakov Azat <azatibrakov@gmail.com>
2020-02-29 15:13:02 +0300

    Add test for 'point_in_polygon' function

--- tests/planar_tests/strategies.py
+++ tests/planar_tests/strategies.py
@@ -3,11 +3,13 @@ from typing import Tuple
 from hypothesis import strategies
 from hypothesis_geometry import planar
 
-from orient.hints import (Coordinate,
+from orient.hints import (Contour,
+                          Coordinate,
                           Point,
                           Segment)
 from tests.strategies import coordinates_strategies
-from tests.utils import Strategy
+from tests.utils import (Strategy,
+                         to_counterclockwise_contour)
 
 segments = coordinates_strategies.flatmap(planar.segments)
 
@@ -19,3 +21,20 @@ def to_segments_with_points(coordinates: Strategy[Coordinate]
 
 
 segments_with_points = coordinates_strategies.flatmap(to_segments_with_points)
+
+
+def to_counterclockwise_contours(coordinates: Strategy[Coordinate]
+                                 ) -> Strategy[Contour]:
+    return planar.contours(coordinates).map(to_counterclockwise_contour)
+
+
+contours = coordinates_strategies.flatmap(to_counterclockwise_contours)
+
+
+def to_contours_with_points(coordinates: Strategy[Coordinate]
+                            ) -> Strategy[Tuple[Contour, Point]]:
+    return strategies.tuples(to_counterclockwise_contours(coordinates),
+                             planar.points(coordinates))
+
+
+contours_with_points = coordinates_strategies.flatmap(to_contours_with_points)
--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,21 @@
 from hypothesis.strategies import SearchStrategy
 
+from orient.core.angular import (Orientation,
+                                 to_orientation)
+from orient.hints import Contour
+
 Strategy = SearchStrategy
 
 
 def implication(antecedent: bool, consequent: bool) -> bool:
     return not antecedent or consequent
+
+
+def to_counterclockwise_contour(contour: Contour) -> Contour:
+    if _to_first_angle_orientation(contour) is not Orientation.CLOCKWISE:
+        contour = contour[:1] + contour[1:][::-1]
+    return contour
+
+
+def _to_first_angle_orientation(contour: Contour) -> Orientation:
+    return to_orientation(contour[-1], contour[0], contour[1])
