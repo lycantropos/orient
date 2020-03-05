4e2ffb5b4d62afebfb5eb81ba53915cb45e7edaf
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-05 19:42:43 +0300

    Complete test for 'contour_in_contour' function

--- tests/planar_tests/test_contour_in_contour.py
+++ tests/planar_tests/test_contour_in_contour.py
@@ -3,7 +3,9 @@ from typing import Tuple
 from hypothesis import given
 
 from orient.hints import Contour
-from orient.planar import contour_in_contour
+from orient.planar import (PointContourLocation,
+                           contour_in_contour,
+                           point_in_contour)
 from tests.utils import (are_contours_similar,
                          implication,
                          to_convex_hull)
@@ -46,3 +48,13 @@ def test_transitivity(contours_triplet: Tuple[Contour, Contour, Contour]
 @given(strategies.contours)
 def test_convex_hull(contour: Contour) -> None:
     assert contour_in_contour(contour, to_convex_hull(contour))
+
+
+@given(strategies.contours_pairs)
+def test_vertices(contours_pair: Tuple[Contour, Contour]) -> None:
+    left_contour, right_contour = contours_pair
+
+    assert implication(contour_in_contour(left_contour, right_contour),
+                       all(point_in_contour(vertex, right_contour)
+                           is not PointContourLocation.OUTSIDE
+                           for vertex in left_contour))
