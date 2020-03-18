02b1498e6e0fc1c13bc4f6498c83ef8043153735
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-18 19:20:11 +0300

    Complete 'contour' module

--- orient/core/contour.py
+++ orient/core/contour.py
@@ -9,27 +9,31 @@ from .sweep import sweep
 
 def contains_contour(goal: Contour, test: Contour) -> bool:
     events_queue = EventsQueue()
-    for segment in to_segments(goal):
-        events_queue.register_segment(segment,
-                                      from_test_contour=False)
-    for segment in to_segments(test):
-        events_queue.register_segment(segment,
-                                      from_test_contour=True)
+    register(events_queue, goal,
+             from_test_contour=False)
+    register(events_queue, test,
+             from_test_contour=True)
     return all(not event.from_test_contour or event.in_intersection
                for event in sweep(events_queue))
 
 
 def contains_contours(goal: Contour, tests: Sequence[Contour]) -> bool:
     events_queue = EventsQueue()
-    for segment in to_segments(goal):
-        events_queue.register_segment(segment,
-                                      from_test_contour=False)
+    register(events_queue, goal,
+             from_test_contour=False)
     for test in tests:
-        for segment in to_segments(test):
-            events_queue.register_segment(segment,
-                                          from_test_contour=True)
+        register(events_queue, test,
+                 from_test_contour=True)
     return all(not event.from_test_contour or event.in_intersection
                for event in sweep(events_queue))
+
+
+def register(events_queue: EventsQueue, contour: Contour,
+             *,
+             from_test_contour: bool) -> None:
+    for segment in to_segments(contour):
+        events_queue.register_segment(segment,
+                                      from_test_contour=from_test_contour)
 
 
 def to_segments(contour: Contour) -> Iterable[Segment]:
