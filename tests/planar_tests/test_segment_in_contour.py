dfd6888a8bc23ef9440973144aa34aa066751372
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-22 01:58:11 +0300

    Complete test for 'segment_in_contour' function

--- tests/planar_tests/test_segment_in_contour.py
+++ tests/planar_tests/test_segment_in_contour.py
@@ -5,7 +5,7 @@ from hypothesis import given
 from orient.core.contour import to_segments
 from orient.hints import (Contour,
                           Segment)
-from orient.planar import segment_in_contour
+from orient.planar import SegmentLocation, segment_in_contour
 from tests.utils import (are_contours_similar,
                          equivalence,
                          to_convex_hull,
@@ -19,17 +19,18 @@ def test_basic(contour_with_segment: Tuple[Contour, Segment]) -> None:
 
     result = segment_in_contour(segment, contour)
 
-    assert isinstance(result, bool)
+    assert isinstance(result, SegmentLocation)
 
 
 @given(strategies.contours)
 def test_edges(contour: Contour) -> None:
-    assert all(segment_in_contour(edge, contour)
+    assert all(segment_in_contour(edge, contour) is SegmentLocation.BOUNDARY
                for edge in to_segments(contour))
 
 
 @given(strategies.contours)
 def test_convex_contour_criterion(contour: Contour) -> None:
     assert equivalence(all(segment_in_contour(ray, contour)
+                           is SegmentLocation.INSIDE
                            for ray in to_non_edge_rays(contour)),
                        are_contours_similar(contour, to_convex_hull(contour)))
