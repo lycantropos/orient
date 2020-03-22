98d9df5a387c2f6e6ec500479bcfcb6b0bbcf465
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-22 04:12:48 +0300

    Complete test for 'point_in_contour' function

--- tests/planar_tests/test_point_in_contour.py
+++ tests/planar_tests/test_point_in_contour.py
@@ -6,6 +6,7 @@ from orient.hints import (Contour,
                           Point)
 from orient.planar import (PointLocation,
                            point_in_contour)
+from tests.utils import rotate_sequence
 from . import strategies
 
 
@@ -19,6 +20,26 @@ def test_basic(contour_with_point: Tuple[Contour, Point]) -> None:
 
 
 @given(strategies.contours)
-def test_contour(contour: Contour) -> None:
+def test_vertices(contour: Contour) -> None:
     assert all(point_in_contour(vertex, contour) is PointLocation.BOUNDARY
                for vertex in contour)
+
+
+@given(strategies.contours_with_points)
+def test_reversed(contour_with_point: Tuple[Contour, Point]) -> None:
+    contour, point = contour_with_point
+
+    result = point_in_contour(point, contour)
+
+    assert result is point_in_contour(point, contour[::-1])
+
+
+@given(strategies.contours_with_points)
+def test_rotated(contour_with_point: Tuple[Contour, Point]) -> None:
+    contour, point = contour_with_point
+
+    result = point_in_contour(point, contour)
+
+    assert all(result is point_in_contour(point,
+                                          rotate_sequence(contour, offset))
+               for offset in range(1, len(contour)))
