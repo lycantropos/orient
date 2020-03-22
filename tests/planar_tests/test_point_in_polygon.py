765821358636a3394bc64cd95f5d43425b175bc8
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-22 03:49:43 +0300

    Complete test for 'point_in_polygon' function

--- tests/planar_tests/test_point_in_polygon.py
+++ tests/planar_tests/test_point_in_polygon.py
@@ -7,6 +7,7 @@ from orient.hints import (Point,
                           Polygon)
 from orient.planar import (PointLocation,
                            point_in_polygon)
+from tests.utils import implication
 from . import strategies
 
 
@@ -24,3 +25,21 @@ def test_vertices(polygon: Polygon) -> None:
     border, holes = polygon
     assert all(point_in_polygon(vertex, polygon) is PointLocation.BOUNDARY
                for vertex in chain(border, *holes))
+
+
+@given(strategies.polygons_with_points)
+def test_solid(polygon_with_point: Tuple[Polygon, Point]) -> None:
+    polygon, point = polygon_with_point
+
+    result = point_in_polygon(point, polygon)
+
+    border, holes = polygon
+    assert implication(result is PointLocation.INSIDE,
+                       point_in_polygon(point, (border, []))
+                       is PointLocation.INSIDE)
+    assert implication(point_in_polygon(point, (border, []))
+                       is PointLocation.OUTSIDE,
+                       result is PointLocation.OUTSIDE)
+    assert implication(point_in_polygon(point, (border, []))
+                       is PointLocation.BOUNDARY,
+                       result is PointLocation.BOUNDARY)
