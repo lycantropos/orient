bfd6bb6c15c24c96b8f01bde6b20552aa608e518
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-23 21:44:57 +0300

    Complete 'polygon.contains_polygon' function

--- orient/core/polygon.py
+++ orient/core/polygon.py
@@ -48,27 +48,27 @@ def contains_segment(polygon: Polygon, segment: Segment) -> SegmentLocation:
 def contains_polygon(goal: Polygon, test: Polygon) -> bool:
     goal_border, goal_holes = goal
     test_border, test_holes = test
-    test_bounding_box = bounding_box.from_points(test_border)
-    if not (bounding_box.contains_bounding_box(
-            bounding_box.from_points(goal_border), test_bounding_box)
-            and contour_contains_contour(goal_border, test_border)):
+    if not contour_contains_contour(goal_border, test_border):
         return False
+    # we are checking that test polygon holes are supersets
+    # of goal polygon holes which lie within test polygon border
+    _, test_max_x, _, _ = bounding_box.from_points(test_border)
     events_queue = EventsQueue()
-    register(events_queue, goal,
-             from_test_contour=False)
-    register(events_queue, test,
-             from_test_contour=True)
-    _, test_max_x, _, _ = test_bounding_box
+    for goal_hole in goal_holes:
+        register_contour(events_queue, goal_hole,
+                         from_test_contour=False)
+    register_contour(events_queue, test_border,
+                     from_test_contour=True)
+    goal_holes_edges_in_test_border = [
+        event.segment
+        for event in sweep(events_queue, test_max_x)
+        if not event.from_test_contour and event.in_intersection]
+    events_queue.clear()
+    for test_hole in test_holes:
+        register_contour(events_queue, test_hole,
+                         from_test_contour=False)
+    for edge in goal_holes_edges_in_test_border:
+        events_queue.register_segment(edge,
+                                      from_test_contour=True)
     return all(not event.from_test_contour or event.in_intersection
                for event in sweep(events_queue, test_max_x))
-
-
-def register(events_queue: EventsQueue, polygon: Polygon,
-             *,
-             from_test_contour: bool) -> None:
-    border, holes = polygon
-    register_contour(events_queue, border,
-                     from_test_contour=from_test_contour)
-    for hole in holes:
-        register_contour(events_queue, hole,
-                         from_test_contour=from_test_contour)
