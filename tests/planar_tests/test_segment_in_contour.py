f091c96a64d7bc6e37478bd0acc441176ae98a18
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-09 13:02:49 +0300

    Complete test for 'segment_in_contour' function

--- tests/planar_tests/test_segment_in_contour.py
+++ tests/planar_tests/test_segment_in_contour.py
@@ -5,8 +5,7 @@ from hypothesis import given
 from orient.core.contour import edges
 from orient.hints import (Contour,
                           Segment)
-from orient.planar import (PointLocation,
-                           SegmentLocation,
+from orient.planar import (Relation,
                            point_in_contour,
                            segment_in_contour)
 from tests.utils import (are_contours_similar,
@@ -23,7 +22,7 @@ def test_basic(contour_with_segment: Tuple[Contour, Segment]) -> None:
 
     result = segment_in_contour(segment, contour)
 
-    assert isinstance(result, SegmentLocation)
+    assert isinstance(result, Relation)
 
 
 @given(strategies.contours_with_segments)
@@ -33,31 +32,29 @@ def test_outside(contour_with_segment: Tuple[Contour, Segment]) -> None:
     result = segment_in_contour(segment, contour)
 
     start, end = segment
-    assert implication(result is SegmentLocation.EXTERNAL,
+    assert implication(result is Relation.DISJOINT,
                        point_in_contour(start, contour)
-                       is PointLocation.EXTERNAL
+                       is Relation.DISJOINT
                        and point_in_contour(end, contour)
-                       is PointLocation.EXTERNAL)
+                       is Relation.DISJOINT)
 
 
 @given(strategies.contours)
 def test_edges(contour: Contour) -> None:
-    assert all(segment_in_contour(edge, contour) is SegmentLocation.BOUNDARY
+    assert all(segment_in_contour(edge, contour) is Relation.COMPONENT
                for edge in edges(contour))
 
 
 @given(strategies.contours)
 def test_contour_separators(contour: Contour) -> None:
     assert all(segment_in_contour(segment, contour)
-               in (SegmentLocation.TOUCH,
-                   SegmentLocation.CROSS,
-                   SegmentLocation.ENCLOSED)
+               in (Relation.TOUCH, Relation.CROSS, Relation.ENCLOSED)
                for segment in to_contour_separators(contour))
 
 
 @given(strategies.contours)
 def test_convex_contour_criterion(contour: Contour) -> None:
     assert equivalence(all(segment_in_contour(segment, contour)
-                           is SegmentLocation.ENCLOSED
+                           is Relation.ENCLOSED
                            for segment in to_contour_separators(contour)),
                        are_contours_similar(contour, to_convex_hull(contour)))
