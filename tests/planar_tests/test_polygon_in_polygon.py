a3009a23f080ee1cadfb7efd62d62577d5039d8f
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-10 17:43:49 +0300

    Complete test for 'polygon_in_polygon' function

--- tests/planar_tests/test_polygon_in_polygon.py
+++ tests/planar_tests/test_polygon_in_polygon.py
@@ -7,7 +7,7 @@ from orient.hints import Polygon
 from orient.planar import (Relation,
                            point_in_polygon,
                            polygon_in_polygon)
-from tests.utils import (are_polygons_similar,
+from tests.utils import (equivalence,
                          implication,
                          to_convex_hull)
 from . import strategies
@@ -19,45 +19,55 @@ def test_basic(polygons_pair: Tuple[Polygon, Polygon]) -> None:
 
     result = polygon_in_polygon(left_polygon, right_polygon)
 
-    assert isinstance(result, bool)
+    assert isinstance(result, Relation)
 
 
 @given(strategies.polygons)
-def test_reflexivity(polygon: Polygon) -> None:
-    assert polygon_in_polygon(polygon, polygon)
+def test_self(polygon: Polygon) -> None:
+    assert polygon_in_polygon(polygon, polygon) is Relation.EQUAL
 
 
 @given(strategies.polygons_pairs)
-def test_antisymmetry(polygons_pair: Tuple[Polygon, Polygon]) -> None:
+def test_symmetric_relations(polygons_pair: Tuple[Polygon, Polygon]) -> None:
     left_polygon, right_polygon = polygons_pair
 
-    assert implication(polygon_in_polygon(left_polygon, right_polygon)
-                       and polygon_in_polygon(right_polygon, left_polygon),
-                       are_polygons_similar(left_polygon, right_polygon))
+    result = polygon_in_polygon(left_polygon, right_polygon)
+
+    complement = polygon_in_polygon(right_polygon, left_polygon)
+    assert equivalence(result is complement,
+                       result in (Relation.DISJOINT, Relation.TOUCH,
+                                  Relation.OVERLAP, Relation.EQUAL))
 
 
-@given(strategies.polygons_triplets)
-def test_transitivity(polygons_triplet: Tuple[Polygon, Polygon, Polygon]
-                      ) -> None:
-    left_polygon, mid_polygon, right_polygon = polygons_triplet
+@given(strategies.polygons_pairs)
+def test_asymmetric_relations(polygons_pair: Tuple[Polygon, Polygon]) -> None:
+    left_polygon, right_polygon = polygons_pair
 
-    assert implication(polygon_in_polygon(left_polygon, mid_polygon)
-                       and polygon_in_polygon(mid_polygon, right_polygon),
-                       polygon_in_polygon(left_polygon, right_polygon))
+    result = polygon_in_polygon(left_polygon, right_polygon)
+
+    complement = polygon_in_polygon(right_polygon, left_polygon)
+    assert equivalence(result is Relation.COVER, complement is Relation.WITHIN)
+    assert equivalence(result is Relation.ENCLOSES,
+                       complement is Relation.ENCLOSED)
+    assert equivalence(result is Relation.COMPOSITE,
+                       complement is Relation.COMPONENT)
 
 
 @given(strategies.polygons)
 def test_border_convex_hull(polygon: Polygon) -> None:
     border, holes = polygon
-    assert polygon_in_polygon(polygon, (to_convex_hull(border), holes))
+    assert (polygon_in_polygon(polygon, (to_convex_hull(border), holes))
+            in (Relation.EQUAL, Relation.ENCLOSED))
 
 
 @given(strategies.polygons)
 def test_without_holes(polygon: Polygon) -> None:
     border, holes = polygon
     polygon_without_holes = (border, [])
-    assert polygon_in_polygon(polygon, polygon_without_holes)
-    assert (not holes) is polygon_in_polygon(polygon_without_holes, polygon)
+    assert (polygon_in_polygon(polygon, polygon_without_holes)
+            is (Relation.ENCLOSED if holes else Relation.EQUAL))
+    assert (polygon_in_polygon(polygon_without_holes, polygon)
+            is (Relation.ENCLOSES if holes else Relation.EQUAL))
 
 
 @given(strategies.polygons_pairs)
@@ -65,7 +75,9 @@ def test_vertices(polygons_pair: Tuple[Polygon, Polygon]) -> None:
     left_polygon, right_polygon = polygons_pair
 
     left_border, left_holes = left_polygon
-    assert implication(polygon_in_polygon(left_polygon, right_polygon),
+    assert implication(polygon_in_polygon(left_polygon, right_polygon)
+                       in (Relation.EQUAL, Relation.COMPONENT,
+                           Relation.ENCLOSES, Relation.WITHIN),
                        all(point_in_polygon(vertex, right_polygon)
                            is not Relation.DISJOINT
                            for vertex in chain(left_border, *left_holes)))
