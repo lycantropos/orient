82d6024ca36393ae2c20c78c57cf0ae6457b1cd1
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-11 19:42:48 +0300

    Complete test for 'segment_in_region' function

--- tests/planar_tests/test_segment_in_region.py
+++ tests/planar_tests/test_segment_in_region.py
@@ -11,6 +11,9 @@ from orient.planar import (Relation,
                            segment_in_region)
 from tests.utils import (equivalence,
                          implication,
+                         reverse_contour,
+                         reverse_segment,
+                         rotations,
                          to_contour_separators,
                          to_convex_hull)
 from . import strategies
@@ -56,3 +59,31 @@ def test_convex_contour_criterion(region: Region) -> None:
                            is Relation.ENCLOSED
                            for segment in to_contour_separators(region)),
                        equal(region, to_convex_hull(region)))
+
+
+@given(strategies.contours_with_segments)
+def test_reversed_segment(region_with_segment: Tuple[Region, Segment]) -> None:
+    region, segment = region_with_segment
+
+    result = segment_in_region(segment, region)
+
+    assert result is segment_in_region(reverse_segment(segment), region)
+
+
+@given(strategies.contours_with_segments)
+def test_reversed_region(region_with_segment: Tuple[Region, Segment]) -> None:
+    region, segment = region_with_segment
+
+    result = segment_in_region(segment, region)
+
+    assert result is segment_in_region(segment, reverse_contour(region))
+
+
+@given(strategies.contours_with_segments)
+def test_rotations(region_with_segment: Tuple[Region, Segment]) -> None:
+    region, segment = region_with_segment
+
+    result = segment_in_region(segment, region)
+
+    assert all(result is segment_in_region(segment, rotated)
+               for rotated in rotations(region))
