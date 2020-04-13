fc86ec32ce90b32a17909d508083413dd8960fba
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-13 14:03:52 +0300

    Complete 'contour_in_multiregion' function

--- orient/core/multiregion.py
+++ orient/core/multiregion.py
@@ -6,10 +6,11 @@ from orient.hints import (Contour,
                           Region,
                           Segment)
 from . import bounding_box
+from .contour import register as register_contour
 from .events_queue import EventsQueue
-from .processing import process_compound_queue
+from .processing import (process_compound_queue,
+                         process_linear_compound_queue)
 from .region import (register as register_region,
-                     relate_contour as relate_contour_to_region,
                      relate_point as relate_point_to_region,
                      relate_segment as relate_segment_to_region)
 from .relation import Relation
@@ -40,19 +41,21 @@ def relate_segment(multiregion: Multiregion, segment: Segment) -> Relation:
 
 
 def relate_contour(multiregion: Multiregion, contour: Contour) -> Relation:
-    do_not_touch = True
-    for region in multiregion:
-        relation_with_region = relate_contour_to_region(region, contour)
-        if relation_with_region in (Relation.CROSS,
-                                    Relation.COMPONENT,
-                                    Relation.ENCLOSED,
-                                    Relation.WITHIN):
-            return relation_with_region
-        elif do_not_touch and relation_with_region is Relation.TOUCH:
-            do_not_touch = False
-    return (Relation.DISJOINT
-            if do_not_touch
-            else Relation.TOUCH)
+    test_bounding_box = bounding_box.from_points(contour)
+    overlapping_multiregion = [
+        region
+        for region in multiregion
+        if not bounding_box.disjoint_with(bounding_box.from_points(region),
+                                          test_bounding_box)]
+    if not overlapping_multiregion:
+        return Relation.DISJOINT
+    events_queue = EventsQueue()
+    register(events_queue, overlapping_multiregion,
+             from_test=False)
+    register_contour(events_queue, contour,
+                     from_test=True)
+    _, test_max_x, _, _ = test_bounding_box
+    return process_linear_compound_queue(events_queue, test_max_x)
 
 
 def relate_region(multiregion: Multiregion, region: Region) -> Relation:
