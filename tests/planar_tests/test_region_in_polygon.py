f12a5bb9a5e0f986b15d03cba98bcfad67a2c4bb
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-13 17:37:20 +0300

    Complete test for 'region_in_polygon' function

--- tests/planar_tests/test_region_in_polygon.py
+++ tests/planar_tests/test_region_in_polygon.py
@@ -5,8 +5,10 @@ from hypothesis import given
 from orient.hints import (Polygon,
                           Region)
 from orient.planar import (Relation,
+                           contour_in_polygon,
                            region_in_polygon)
-from tests.utils import COMPOUND_RELATIONS
+from tests.utils import (COMPOUND_RELATIONS,
+                         equivalence)
 from . import strategies
 
 
@@ -23,3 +25,30 @@ def test_basic(region_with_polygon: Tuple[Polygon, Region]) -> None:
 @given(strategies.contours)
 def test_self(region: Region) -> None:
     assert region_in_polygon(region, (region, [])) is Relation.EQUAL
+
+
+@given(strategies.polygons_with_contours)
+def test_connection_with_contour_in_polygon(polygon_with_region
+                                            : Tuple[Polygon, Region]
+                                            ) -> None:
+    polygon, region = polygon_with_region
+
+    result = region_in_polygon(region, polygon)
+
+    contour_relation = contour_in_polygon(region, polygon)
+    assert equivalence(result is Relation.DISJOINT
+                       or result is Relation.COVER,
+                       contour_relation is Relation.DISJOINT)
+    assert equivalence(result is Relation.TOUCH
+                       or result is Relation.ENCLOSES
+                       or result is Relation.COMPOSITE,
+                       contour_relation is Relation.TOUCH)
+    assert equivalence(result is Relation.OVERLAP,
+                       contour_relation is Relation.CROSS)
+    assert equivalence(result is Relation.COMPONENT
+                       or result is Relation.EQUAL,
+                       contour_relation is Relation.COMPONENT)
+    assert equivalence(result is Relation.ENCLOSED,
+                       contour_relation is Relation.ENCLOSED)
+    assert equivalence(result is Relation.WITHIN,
+                       contour_relation is Relation.WITHIN)
