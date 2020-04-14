dee58555787638566e8a753030affb3d0d4fb2d0
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-14 12:50:55 +0300

    Complete 'processing' module

--- orient/core/processing.py
+++ orient/core/processing.py
@@ -13,26 +13,20 @@ def process_linear_queue(events_queue: EventsQueue,
     test_boundary_in_goal_interior = goal_boundary_in_test_interior = False
     has_overlap = has_cross = False
     for event in sweep(events_queue, test_max_x):
-        if (test_is_subset_of_goal and event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
+        if (test_is_subset_of_goal
+                and event.from_test and event.outside):
             test_is_subset_of_goal = False
-        if (goal_is_subset_of_test and not event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
+        if (goal_is_subset_of_test
+                and event.from_goal and event.outside):
             goal_is_subset_of_test = False
         if (boundaries_do_not_intersect
                 and event.relationship is not SegmentsRelationship.NONE):
             boundaries_do_not_intersect = False
-        if (not test_boundary_in_goal_interior and event.from_test
-                and event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
+        if (not test_boundary_in_goal_interior
+                and event.from_test and event.inside):
             test_boundary_in_goal_interior = True
-        if (not goal_boundary_in_test_interior and not event.from_test
-                and event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
+        if (not goal_boundary_in_test_interior
+                and event.from_goal and event.inside):
             goal_boundary_in_test_interior = True
         if (not has_overlap
                 and event.relationship is SegmentsRelationship.OVERLAP):
@@ -69,34 +63,22 @@ def process_linear_compound_queue(events_queue: EventsQueue,
                                   test_max_x: Coordinate) -> Relation:
     # ``goal`` is a compound object
     # ``test`` is a linear object
-    has_cross = has_touch = False
-    test_boundary_in_goal_interior = test_boundary_in_goal_exterior = False
+    test_boundary_in_goal_interior = has_touch = False
     test_is_subset_of_goal = goal_is_subset_of_test = True
     for event in sweep(events_queue, test_max_x):
-        if (not test_boundary_in_goal_interior and event.from_test
-                and event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
-            test_boundary_in_goal_interior = True
-        if (not test_boundary_in_goal_exterior and event.from_test
-                and not event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
-            test_boundary_in_goal_exterior = True
-        if not has_cross and event.relationship is SegmentsRelationship.CROSS:
-            has_cross = True
-        if (not has_touch
-                and event.relationship in (SegmentsRelationship.TOUCH,
-                                           SegmentsRelationship.OVERLAP)):
-            has_touch = True
-        if (test_is_subset_of_goal and event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
+        if event.relationship is SegmentsRelationship.CROSS:
+            return Relation.CROSS
+        if test_is_subset_of_goal and event.from_test and event.outside:
             test_is_subset_of_goal = False
-        if (goal_is_subset_of_test and not event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
+        if goal_is_subset_of_test and event.from_goal and event.outside:
             goal_is_subset_of_test = False
+        if (not test_boundary_in_goal_interior
+                and event.from_test and event.inside):
+            test_boundary_in_goal_interior = True
+        if (not has_touch
+                and (event.relationship is SegmentsRelationship.TOUCH
+                     or event.relationship is SegmentsRelationship.OVERLAP)):
+            has_touch = True
     if goal_is_subset_of_test:
         goal_is_subset_of_test = not events_queue
     if goal_is_subset_of_test:
@@ -111,8 +93,7 @@ def process_linear_compound_queue(events_queue: EventsQueue,
                 else Relation.WITHIN)
     else:
         return (Relation.CROSS
-                if has_cross or (test_boundary_in_goal_interior
-                                 and test_boundary_in_goal_exterior)
+                if test_boundary_in_goal_interior
                 else (Relation.TOUCH
                       if has_touch
                       else Relation.DISJOINT))
@@ -121,35 +102,26 @@ def process_linear_compound_queue(events_queue: EventsQueue,
 def process_compound_queue(events_queue: EventsQueue,
                            test_max_x: Coordinate) -> Relation:
     test_boundary_in_goal_interior = goal_boundary_in_test_interior = False
-    boundaries_do_not_intersect, overlaps = True, False
+    boundaries_do_not_intersect, boundary_in_other_interior = True, False
     test_is_subset_of_goal = goal_is_subset_of_test = True
     for event in sweep(events_queue, test_max_x):
         if event.relationship is SegmentsRelationship.CROSS:
             return Relation.OVERLAP
+        if test_is_subset_of_goal and event.from_test and event.outside:
+            test_is_subset_of_goal = False
+        if goal_is_subset_of_test and event.from_goal and event.outside:
+            goal_is_subset_of_test = False
         if (boundaries_do_not_intersect
                 and event.relationship is not SegmentsRelationship.NONE):
             boundaries_do_not_intersect = False
-        if (not overlaps and event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
-            overlaps = True
-        if (not test_boundary_in_goal_interior and event.from_test
-                and event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
+        if not boundary_in_other_interior and event.inside:
+            boundary_in_other_interior = True
+        if (not test_boundary_in_goal_interior
+                and event.from_test and event.inside):
             test_boundary_in_goal_interior = True
-        if (not goal_boundary_in_test_interior and not event.from_test
-                and event.in_intersection
-                and event.relationship in (SegmentsRelationship.NONE,
-                                           SegmentsRelationship.TOUCH)):
+        if (not goal_boundary_in_test_interior
+                and event.from_goal and event.inside):
             goal_boundary_in_test_interior = True
-        if (test_is_subset_of_goal and event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
-            test_is_subset_of_goal = False
-        if (goal_is_subset_of_test and not event.from_test
-                and not event.in_intersection
-                and event.relationship is not SegmentsRelationship.OVERLAP):
-            goal_is_subset_of_test = False
     if goal_is_subset_of_test:
         goal_is_subset_of_test = not events_queue
     if boundaries_do_not_intersect:
@@ -158,7 +130,7 @@ def process_compound_queue(events_queue: EventsQueue,
                 else (Relation.COVER
                       if goal_is_subset_of_test
                       else (Relation.OVERLAP
-                            if overlaps
+                            if boundary_in_other_interior
                             else Relation.DISJOINT)))
     elif test_is_subset_of_goal:
         return (Relation.EQUAL
@@ -172,5 +144,5 @@ def process_compound_queue(events_queue: EventsQueue,
                 else Relation.COMPOSITE)
     else:
         return (Relation.OVERLAP
-                if overlaps
+                if boundary_in_other_interior
                 else Relation.TOUCH)
