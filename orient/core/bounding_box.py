40b3b699839c8c7c3fe485582343a4738c7ebc9b
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-15 22:35:17 +0300

    Complete 'bounding_box' module

--- orient/core/bounding_box.py
+++ orient/core/bounding_box.py
@@ -13,13 +13,6 @@ def contains_point(bounding_box: BoundingBox, point: Point) -> bool:
     return x_min <= point_x <= x_max and y_min <= point_y <= y_max
 
 
-def contains_bounding_box(goal: BoundingBox, test: BoundingBox) -> bool:
-    ((goal_x_min, goal_x_max, goal_y_min, goal_y_max),
-     (test_x_min, test_x_max, test_y_min, test_y_max)) = goal, test
-    return (goal_x_min <= test_x_min and test_x_max <= goal_x_max
-            and goal_y_min <= test_y_min and test_y_max <= goal_y_max)
-
-
 def disjoint_with(goal: BoundingBox, test: BoundingBox) -> bool:
     ((goal_x_min, goal_x_max, goal_y_min, goal_y_max),
      (test_x_min, test_x_max, test_y_min, test_y_max)) = goal, test
