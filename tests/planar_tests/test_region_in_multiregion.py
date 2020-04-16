38e882ffb7728f8108add08c95e575f984d15ff6
Azat Ibrakov <azatibrakov@gmail.com>
2020-04-16 09:12:37 +0300

    Complete test for 'region_in_multiregion' function

--- tests/planar_tests/test_region_in_multiregion.py
+++ tests/planar_tests/test_region_in_multiregion.py
@@ -51,53 +51,53 @@ def test_base(multiregion_with_region: Tuple[Multiregion, Region]) -> None:
 @given(strategies.non_empty_multicontours_with_contours)
 def test_step(multiregion_with_region: Tuple[Multiregion, Region]) -> None:
     multiregion, region = multiregion_with_region
-    first_contour, *rest_multiregion = multiregion
+    first_region, *rest_multiregion = multiregion
 
     result = region_in_multiregion(region, rest_multiregion)
     next_result = region_in_multiregion(region, multiregion)
 
-    relation_with_first_contour = region_in_region(region, first_contour)
+    relation_with_first_region = region_in_region(region, first_region)
     assert equivalence(next_result is Relation.DISJOINT,
-                       result is relation_with_first_contour
+                       result is relation_with_first_region
                        is Relation.DISJOINT)
     assert equivalence(next_result is Relation.TOUCH,
                        result is Relation.DISJOINT
-                       and relation_with_first_contour is Relation.TOUCH
+                       and relation_with_first_region is Relation.TOUCH
                        or result is Relation.TOUCH
-                       and relation_with_first_contour in (Relation.DISJOINT,
+                       and relation_with_first_region in (Relation.DISJOINT,
                                                            Relation.TOUCH))
     assert equivalence(next_result is Relation.COMPONENT,
                        result is Relation.COMPONENT
                        or bool(rest_multiregion)
-                       and relation_with_first_contour is Relation.EQUAL)
+                       and relation_with_first_region is Relation.EQUAL)
     assert equivalence(next_result is Relation.OVERLAP,
                        result is Relation.OVERLAP
-                       or relation_with_first_contour is Relation.OVERLAP
+                       or relation_with_first_region is Relation.OVERLAP
                        or (bool(rest_multiregion)
                            and result is Relation.DISJOINT
                            or result is Relation.TOUCH)
-                       and relation_with_first_contour in (Relation.COVER,
+                       and relation_with_first_region in (Relation.COVER,
                                                            Relation.ENCLOSES)
                        or result in (Relation.COVER, Relation.ENCLOSES)
-                       and relation_with_first_contour is Relation.DISJOINT)
+                       and relation_with_first_region is Relation.DISJOINT)
     assert equivalence(next_result is Relation.COVER,
                        (not rest_multiregion or result is Relation.COVER)
-                       and relation_with_first_contour is Relation.COVER)
+                       and relation_with_first_region is Relation.COVER)
     assert equivalence(next_result is Relation.ENCLOSES,
                        result is Relation.ENCLOSES
-                       and relation_with_first_contour in (Relation.ENCLOSES,
+                       and relation_with_first_region in (Relation.ENCLOSES,
                                                            Relation.COVER)
                        or (not rest_multiregion or result is Relation.COVER)
-                       and relation_with_first_contour is Relation.ENCLOSES)
+                       and relation_with_first_region is Relation.ENCLOSES)
     assert equivalence(next_result is Relation.EQUAL,
                        not rest_multiregion
-                       and relation_with_first_contour is Relation.EQUAL)
+                       and relation_with_first_region is Relation.EQUAL)
     assert equivalence(next_result is Relation.ENCLOSED,
                        result is Relation.ENCLOSED
-                       or relation_with_first_contour is Relation.ENCLOSED)
+                       or relation_with_first_region is Relation.ENCLOSED)
     assert equivalence(next_result is Relation.WITHIN,
                        result is Relation.WITHIN
-                       or relation_with_first_contour is Relation.WITHIN)
+                       or relation_with_first_region is Relation.WITHIN)
 
 
 @given(strategies.multicontours_with_contours)
