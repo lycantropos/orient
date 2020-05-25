24dfd285c07da4e8f6e6b80d29fcc53dc9ab314c
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-25 19:28:55 +0300

    Complete 'contour_in_polygon' function

--- orient/core/polygon.py
+++ orient/core/polygon.py
@@ -53,12 +53,12 @@ def relate_segment(polygon: Polygon, segment: Segment) -> Relation:
 
 def relate_contour(polygon: Polygon, contour: Contour) -> Relation:
     border, holes = polygon
-    relation_with_border = relate_contour_to_region(border, contour)
-    if holes and (relation_with_border is Relation.ENCLOSED
-                  or relation_with_border is Relation.WITHIN):
+    relation_without_holes = relate_contour_to_region(border, contour)
+    if holes and (relation_without_holes is Relation.ENCLOSED
+                  or relation_without_holes is Relation.WITHIN):
         relation_with_holes = relate_contour_to_multiregion(holes, contour)
         if relation_with_holes is Relation.DISJOINT:
-            return relation_with_border
+            return relation_without_holes
         elif relation_with_holes is Relation.TOUCH:
             return Relation.ENCLOSED
         elif (relation_with_holes is Relation.CROSS
@@ -70,7 +70,7 @@ def relate_contour(polygon: Polygon, contour: Contour) -> Relation:
             # contour is within holes
             return Relation.DISJOINT
     else:
-        return relation_with_border
+        return relation_without_holes
 
 
 def relate_region(polygon: Polygon, region: Region) -> Relation:
