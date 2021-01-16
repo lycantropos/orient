931ba07a7be9274b80c34e072cf62065aa5db515
Azat Ibrakov <azatibrakov@gmail.com>
2021-01-16 03:00:50 +0300

    Complete type hints

--- orient/core/hints.py
+++ orient/core/hints.py
@@ -1,11 +1,9 @@
 from typing import (Sequence,
                     Tuple)
 
-from ground.hints import (Point as _Point,
-                          Segment as _Segment)
+from ground.hints import Point as _Point
 
 SegmentEndpoints = Tuple[_Point, _Point]
-Multisegment = Sequence[_Segment]
 Contour = Region = Sequence[_Point]
 Multiregion = Sequence[Region]
 Polygon = Tuple[Region, Multiregion]
